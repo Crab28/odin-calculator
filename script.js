683a333326554979392257c030bed27adb1309be
Crab <crabcodes28@gmail.com>
2023-05-25 22:21:55 +0200

    Add calculation functions and variables

---
+++ script.js
@@ -0,0 +1,18 @@
+let sum = 0;
+let operator = null;
+
+function add(...args) {
+    return args[0] + args[1];
+}
+
+function subtract(...args) {
+    return args[0] - args[1];
+}
+
+function multiply(...args) {
+    return args[0] * args[1];
+}
+
+function divide(...args) {
+    return args[0] / args[1];
+}
