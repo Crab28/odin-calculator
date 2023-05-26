3a17226ab444ca57acad28ce3f72e39a6d26e768
Crab <crabcodes28@gmail.com>
2023-05-26 03:32:19 +0200

    Add html structure
    JS function is being used to generate the button elements

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 let sum = 0;
 let operator = null;
 
+function createCalculatorKeys() {
+    const CALC_KEYS = document.getElementById('calculator-keys');
+    const ROWS = 4;
+    let keys = [
+        ['1', '2', '3', '/'], 
+        ['4', '5', '6', 'x'],
+        ['7', '8', '9', '-'],
+        ['.', '0', '=', '+']
+    ];
+
+    for (let i = 0; i < ROWS; i++) {
+        const NEW_ROW = document.createElement('row');
+        NEW_ROW.classList.add('keys-row');
+        
+        for (let n = 0; n < keys[i].length; n++) {
+            const NEW_BUTTON = document.createElement('button');
+            NEW_BUTTON.classList.add('btn');
+            NEW_BUTTON.textContent = keys[i][n];
+            NEW_ROW.appendChild(NEW_BUTTON);
+        }
+
+        CALC_KEYS.appendChild(NEW_ROW)
+    }
+}
+
 function add(nextNum) {
     return sum + nextNum;
 }
@@ -29,3 +54,5 @@ function operate(nextNum, operator) {
             sum = divide(nextNum);
     }
 }
+
+createCalculatorKeys();
