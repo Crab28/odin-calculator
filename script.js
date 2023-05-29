888d3896a1cc57c0e97da2c8523a31b345d04e2c
Crab <crabcodes28@gmail.com>
2023-05-29 23:21:28 +0200

    Add functionality to number buttons

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let sum = 0;
+const CALCULATOR_WINDOW = document.getElementById('calculator-window');
+
+let firstNum = '0';
+let secondNum = null;
 let operator = null;
+let decimalUsed = false;
 
 function createCalculatorKeys() {
     const CALC_KEYS = document.getElementById('calculator-keys');
@@ -20,26 +24,84 @@ function createCalculatorKeys() {
             NEW_BUTTON.classList.add('btn');
             NEW_BUTTON.textContent = keys[i][n];
             NEW_ROW.appendChild(NEW_BUTTON);
+
+            addButtonListener(NEW_BUTTON);
         }
 
         CALC_KEYS.appendChild(NEW_ROW)
     }
 }
 
+function addButtonListener(button) {
+    const OPERATORS = ['+', '-', 'x', '/'];
+    const windowText = button.textContent;
+
+    button.addEventListener('click', () => {
+        if (OPERATORS.includes(windowText)) {
+            return;
+        }
+        else if (windowText === '=') {
+            return;
+        }
+        else if (windowText === '.') {
+            return;
+        }
+        else {
+            updateWindowText(windowText);
+        }
+    })
+}
+
+function updateWindowText(windowText) {
+    if (operator === null) {
+        if (firstNum === '0') {
+            firstNum = '';
+        }
+
+        firstNum += windowText;
+        CALCULATOR_WINDOW.textContent = firstNum;
+    }
+}
+
+function calculateSum() {
+    const CURRENT_NUM = CALCULATOR_WINDOW.textContent;
+
+    switch (operator) {
+        case '+':
+            sum = add(CURRENT_NUM);
+            CALCULATOR_WINDOW.textContent = sum;
+            break;
+        case '-':
+            sum = subtract(CURRENT_NUM);
+            CALCULATOR_WINDOW.textContent = sum;
+            break;
+        case 'x':
+            sum = multiply(CURRENT_NUM);
+            CALCULATOR_WINDOW.textContent = sum;
+            break;
+        case '-':
+            sum = divide(CURRENT_NUM);
+            CALCULATOR_WINDOW.textContent = sum;
+            break;
+    }
+
+    secondNum = true;
+}
+
 function add(nextNum) {
-    return sum + nextNum;
+    return  Number(nextNum);
 }
 
 function subtract(nextNum) {
-    return sum - nextNum;
+    return sum - Number(nextNum);
 }
 
 function multiply(nextNum) {
-    return sum * nextNum;
+    return sum * Number(nextNum);
 }
 
 function divide(nextNum) {
-    return sum / nextNum;
+    return sum / Number(nextNum);
 }
 
 function operate(nextNum, operator) {
